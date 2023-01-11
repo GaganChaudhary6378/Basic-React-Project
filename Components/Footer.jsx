f0478c1e169a530355ecc98b04f86504902a73be
Gagan Chaudhary <singhgaganbtp@gmail.com>
2023-01-11 20:07:36 +0530

    facts

---
+++ Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return (
+        <footer className="foot">
+            <p>Made with 💖 by Gagan Chaudhary</p>
+        </footer>
+    )
+}
---
+++ Components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav className="main-head">
+        <img className="nav-logo" src="logo.png" />
+        <h3 className="nav-text">ReactFacts</h3>
+        <h4 className="nav-title">React Project 1</h4>
+      </nav>
+    </header>
+  );
+}
+
+export default Header
